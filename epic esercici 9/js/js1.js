548f5f5390e69b2a9cee652b49933dd92c463117
Rbn444 <106075599+Rbn444@users.noreply.github.com>
2022-06-06 16:20:23 +0200

    Add files via upload

---
+++ epic esercici 9/js/js1.js
@@ -0,0 +1,21 @@
+
+
+window.alert('benvenuto sulla pagina di output js');
+alert('guarda il mio JS');
+
+document.write ("risultati js esterno")
+document.write("<br>")
+var sportPreferito = 'golf';
+console.log(sportPreferito);
+var sportPreferito = 'ping pong';
+console.log(sportPreferito);
+
+document.write("<br>");
+
+document.write("sport preferito:");
+document.write("<br>");
+var sportPreferito1 = 'nuoto';
+document.write(sportPreferito1);
+document.write("<br>")
+document.write("guarda nella console per vedere altri sport peferiti")
+
